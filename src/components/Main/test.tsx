b0820a02b32082af68f0755bdb6b31eb484b05d5
Matheus Boari <matheusboari@gmail.com>
2022-08-31 16:58:23 -0300

    added styles and test colors

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Main from '.';
 
 describe('<Main />', () => {
-  it('Should render the heading', () => {
+  it('should render the heading', () => {
     const { container } = render(<Main />);
 
     expect(
@@ -11,5 +11,11 @@ describe('<Main />', () => {
     ).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
   });
 });
